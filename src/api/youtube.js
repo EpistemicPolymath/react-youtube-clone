3a1500a31091dc2298c5da09fd4642ef23ccc20f
polymathical <epistemicpolymath@gmail.com>
2019-09-11 13:16:15 -0400

    Setting up Youtube API

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,8 @@
 import axios from 'axios'; // https://www.npmjs.com/package/axios
 
 // We will be using the Youtube Data API - https://developers.google.com/youtube/v3/getting-started
+// key=API_KEY = 
 export default axios.create({
-    baseURL: '',
+    baseURL: 'https:/www.googleapis.com/youtube/v3',
     params: ""
 });
